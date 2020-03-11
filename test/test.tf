4dec0b25a2882473b6f7eba33ac780508a984d28
Dzmitry Rudnouski <dzmitry_rudnouski@epam.com>
2020-03-11 13:45:59 +0300

    test

---
+++ test/test.tf
@@ -0,0 +1,33 @@
+module "ecs" {
+  source = ".."
+
+  project     = "Project"
+  environment = "dev"
+  service     = "service-name"
+
+  vpc_id  = "vpc-eizox8ea"
+  subnets = ["subnet-sait0aiw", "subnet-op8phee4", "subnet-eego9xoo"]
+
+  alb_target_group_arn = "arn:aws:elasticloadbalancing:< region >:< account ID >:targetgroup/< target group name >/3b4e9fbf82439af5"
+  container_port       = "80"
+
+  availability_zones = "${var.availability_zones}"
+
+  container_definitions = <<EOF
+[
+  {
+    "name": "first",
+    "image": "service-first",
+    "cpu": 10,
+    "memory": 512,
+    "essential": true,
+    "portMappings": [
+      {
+        "containerPort": 80,
+        "hostPort": 80
+      }
+    ]
+  }
+]
+EOF
+}
